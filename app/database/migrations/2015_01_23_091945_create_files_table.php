29cdf5c77f38457ea9f7636f7e8ad9b4c9b28cb6
Sander Kastelein <sander@sanderkastelein.nl>
2015-01-23 10:25:04 +0100

    Nieuw DB tables

---
+++ app/database/migrations/2015_01_23_091945_create_files_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateFilesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('files', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('owner_id');
+			$table->integer('group_id');
+			$table->boolean('private');
+			
+			$table->timestamps();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('files');
+	}
+
+}
---
+++ app/models/Group.php
@@ -0,0 +1,10 @@
+<?php
+
+Class Group extends Model{
+	
+	
+
+
+
+	
+}
