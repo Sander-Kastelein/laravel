c68620f7665caa7efb0042e413ba20d937c0a20d
Sander-Kastelein <sander@sanderkastelein.nl>
2015-04-12 22:44:20 +0200

    tooltip

--- app/views/components/skills.blade.php
+++ app/views/components/skills.blade.php
@@ -1,10 +1,22 @@
 <h2>Competenties</h2>
 
-@foreach(Skills::all() as $skill)
-<div class="row">
-	<label>{{$skill->name}}</label>
-	<input type="checkbox" {{ ($skill->level($user) >= 1)?"checked":"" }}>
-	<input type="checkbox" {{ ($skill->level($user) >= 2)?"checked":"" }}>
-	<input type="checkbox" {{ ($skill->level($user) >= 3)?"checked":"" }}>
-</div>	
-@endforeach
+<table class="table">
+	<thead>
+		<tr>
+			<th>Competentie</th>
+			<th>Niveau 1</th>
+			<th>Niveau 2</th>
+			<th>Niveau 3</th>
+		</tr>
+	</thead>
+	<tbody>
+		@foreach(Skills::all() as $skill)
+		<tr>
+			<td>{{$skill->name}}</td>
+			<td data-toggle="tooltip" title="{{$skill->description_level_1}}"><input type="checkbox" {{ ($skill->level($user) >= 1)?"checked":"" }}></td>
+			<td data-toggle="tooltip" title="{{$skill->description_level_2}}"><input type="checkbox" {{ ($skill->level($user) >= 2)?"checked":"" }}></td>
+			<td data-toggle="tooltip" title="{{$skill->description_level_3}}"><input type="checkbox" {{ ($skill->level($user) >= 3)?"checked":"" }}></td>
+		</tr>	
+		@endforeach
+	</tbody>
+</table>
