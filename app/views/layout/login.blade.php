8f68f4a5ae059165c8895e8ec65e0c2bb13edcd9
Sander-Kastelein <sander@sanderkastelein.nl>
2015-04-12 18:25:38 +0200

    ssl bugfix

--- app/views/layout/login.blade.php
+++ app/views/layout/login.blade.php
@@ -11,7 +11,7 @@
 	<link rel="stylesheet" type="text/css" href="/css/alertify-bootstrap.min.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<!--webfonts-->
-	<link href='http://fonts.googleapis.com/css?family=Open+Sans:600italic,400,300,600,700' rel='stylesheet' type='text/css'>
+	<link href='//fonts.googleapis.com/css?family=Open+Sans:600italic,400,300,600,700' rel='stylesheet' type='text/css'>
 	<!--//webfonts-->
 	<style type="text/css">
 		html,body{
