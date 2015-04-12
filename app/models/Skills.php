2972ca6e694a6b7a1c9823dac79c73bd49df7223
Sander-Kastelein <sander@sanderkastelein.nl>
2015-04-12 22:39:45 +0200

    bug fix

--- app/models/Skills.php
+++ app/models/Skills.php
@@ -10,7 +10,9 @@ Class Skills extends Eloquent{
 
 
 	public function level($user){
-		return SkillsUser::where('user_id',$user->id)->where('skill_id',$this->id)->first()->level;
+		$su = SkillsUser::where('user_id',$user->id)->where('skill_id',$this->id)->first();
+		if($su) return $su->level;
+		return 0;
 	}
 
 }
